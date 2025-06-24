7a601c0d03f07cb90897617800bdcc621f16b5c6
Aleksandar Stanchev <altryx15@gmail.com>
2025-06-24 22:03:33 +0300

    Updates on structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [serverMessage, setServerMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3001/')
+      .then(response => response.text())
+      .then(data => {
+        setServerMessage(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data from server:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
