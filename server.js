7396551ebe11af14d8bd4e16c39f67707f621abb
Aleksandar Stanchev <altryx15@gmail.com>
2025-07-12 19:46:50 +0300

    identation

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express  = require('express');
+const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { GoogleGenerativeAI } = require('@google/generative-ai');
@@ -19,8 +19,8 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use('/api/recognizeFace', recognizeFaceRoute);
 
 if (!API_KEY) {
-    console.error("GEMINI_API_KEY is not set. Please set it in your environment variables.");
-    process.exit(1);
+  console.error("GEMINI_API_KEY is not set. Please set it in your environment variables.");
+  process.exit(1);
 }
 
 const genAI = new GoogleGenerativeAI(API_KEY);
@@ -32,32 +32,33 @@ app.use((req, res, next) => {
 
 app.post('/login', (req, res) => {
   const { email, password } = req.body;
-  if(email && password) {
+  if (email && password) {
     // Here you would typically check the credentials against a database
     // For simplicity, we are just returning a dummy token
     res.json({ token: 'test123' });
   } else {
     res.status(400).json({ error: 'Email and password are required' });
-}});
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Puuuuuuuu');
 });
 
 app.post('/ask', async (req, res) => {
-const userMessage = req.body.message;
+  const userMessage = req.body.message;
 
-try {
-  const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite' });
-  const result = await model.generateContent(userMessage);
-  const response = await result.response;
-  const text = response.text();
+  try {
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite' });
+    const result = await model.generateContent(userMessage);
+    const response = await result.response;
+    const text = response.text();
 
-  res.json({ reply: text });
-} catch (error) {
-  console.error('Error generating content from Gemini:', error);
-  res.status(500).json({ error: 'Failed to get response from Gemini' });
-}
+    res.json({ reply: text });
+  } catch (error) {
+    console.error('Error generating content from Gemini:', error);
+    res.status(500).json({ error: 'Failed to get response from Gemini' });
+  }
 });
 
 app.use((req, res) => {
